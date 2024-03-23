e0b76100a9390c111772532582b5e13dfa0baba4
Sho Mizushima <sho.mizushima@icloud.com>
2024-03-23 14:38:21 +0900

    [Add]Added frontend Next app.

--- backend/src/main/java/org/sample/backend/Controller.java
+++ backend/src/main/java/org/sample/backend/Controller.java
@@ -1,12 +1,14 @@
 package org.sample.backend;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
     @GetMapping("/get")
-    public String get(){
+    @CrossOrigin(origins = "http://localhost:3000")
+    public String get() {
         return "auth ok";
     }
 }
---
+++ frontend/app/Button.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { FunctionComponent } from "react";
+import axios from "axios";
+
+interface Props {
+  title: string;
+}
+
+const buttonClick = () => {
+  axios
+    .get("http://localhost:8080/get")
+    .then((res) => {
+      alert(res.data);
+    })
+    .catch((err) => {
+      console.log(err);
+      alert(err);
+    });
+};
+
+const Button: FunctionComponent<Props> = ({ title }) => {
+  return (
+    <button
+      onClick={buttonClick}
+      style={{ backgroundColor: "red", color: "yellow" }}
+    >
+      {title}
+    </button>
+  );
+};
+
+export default Button;
